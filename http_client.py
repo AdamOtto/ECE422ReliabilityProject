89b171951aa7f06450b9370d426634b90cea1f44
Adam Otto <aotto@ualberta.ca>
2018-04-08 14:24:18 -0600

    Changed the client to ramp down a little slower.  Added a plot for server size.

--- http_client.py
+++ http_client.py
@@ -11,8 +11,8 @@ if len(sys.argv) < 4:
     print('To few arguments; you need to specify 3 arguments.')
     print('Default values will be used for server_ip, no of users and think time.\n')
     swarm_master_ip = '204.209.76.156'  # ip address of the Swarm master node
-    no_users = 4  # number of concurrent users sending request to the server
-    think_time = 5  # the user think time (seconds) in between consequent requests
+    no_users = 6  # number of concurrent users sending request to the server
+    think_time = 0  # the user think time (seconds) in between consequent requests
 else:
     print('Default values have be overwritten.')
     swarm_master_ip = sys.argv[1]
@@ -30,7 +30,7 @@ class MyThread(threading.Thread):
 
     def run(self):
         print("Starting " + self.name + str(self.counter))
-        workload(self.name + str(self.counter), (self.numUsers - self.counter) * 2 )
+        workload(self.name + str(self.counter), (self.numUsers - self.counter) * 4 )
 
 class workLoadThread(threading.Thread):
     def __init__(self):
@@ -41,7 +41,7 @@ class workLoadThread(threading.Thread):
         global numRequests
         global no_users
         t2 = t0 = time.time()
-        while threadDone < no_users:
+        while threadDone < no_users+1:
             t1 = time.time()
             time.sleep(sleepTime)
             t2 = time.time()
@@ -51,19 +51,73 @@ class workLoadThread(threading.Thread):
             f.close()
             numRequests = 0
 
+def lastTask():
+    global ts
+    for x in range(0, 5):
+        global numRequests
+        numRequests += 1
+        tryOver = True
+        while tryOver == True: #
+            t0 = time.time()
+            try:
+                r = requests.get('http://' + swarm_master_ip + ':8000/')
+                tryOver = False
+            except:
+                tryOver = True
+        t1 = time.time()
+
+        global numRequests
+        numRequests += 1
+
+        rs = r.text.split('.')
+        #print(rs)
+        if isInt(int(rs[3])):
+            f = open('appSize.txt', 'a')
+            f.write(str(t1 - ts) + ',' + rs[3].rstrip() + '\n')
+            f.close()
+        f = open('responseTime.txt', 'a')
+        f.write(str(t1 - ts) + ',' + str(t1 - t0) + '\n')
+        f.close()
+
+        time.sleep(5)
+    global  threadDone
+    threadDone += 1
+
+def isInt(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
 def workload(user, max):
     global ts
     #ts = time.time()
     #while True:
     for x in range(0, max):
-        t0 = time.time()
-        r = requests.get('http://' + swarm_master_ip + ':8000/')
+        #t0 = time.time()
+        global numRequests
+        numRequests += 1
+        tryOver = True
+        while tryOver == True: #
+            t0 = time.time()
+            try:
+                r = requests.get('http://' + swarm_master_ip + ':8000/')
+                tryOver = False
+            except:
+                tryOver = True
         #print(r.text)
         t1 = time.time()
 
         global numRequests
         numRequests += 1
 
+        rs = r.text.split('.')
+        #print(rs)
+        if isInt(int(rs[3])):
+            f = open('appSize.txt', 'a')
+            f.write(str(t1 - ts) + ',' + rs[3].rstrip() + '\n')
+            f.close()
         f = open('responseTime.txt', 'a')
         f.write(str(t1 - ts) + ',' + str(t1 - t0) + '\n')
         f.close()
@@ -87,6 +141,8 @@ if __name__ == "__main__":
     ts = time.time()
     for i in range(no_users):
         threads[i].start()
-        time.sleep(5)
+        time.sleep(10)
     for i in range(no_users):
         threads[i].join()
+
+    lastTask()
